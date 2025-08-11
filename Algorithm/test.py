e33b31a5b84082e009a13eb64a964fb3c00d7a02
yechan-EEEEE <yechan7530@gmail.com>
2025-08-11 17:59:14 +0900

    edit_250811_2

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -176,17 +176,26 @@ v 1 2 3 4 5
 6262 6004 1801 7660 7919 1280 525 9798 5134 1821                                                        #2 11088
 20 19
 3266 9419 3087 9001 9321 1341 7379 6236 5795 8910 2990 2152 2249 4059 1394 6871 4911 3648 1969 2176     #3 1090
-'''
-# import sys
-# sys.stdin = open("input.txt","r")
-#
-# T=int(input())
-# for test_case in range(1,T+1):
-#     N, M = list(map(int,input().split()))
-#     num = list(map(int,input().split()))
-#     first_sum = 0
-#     for ai in range(M):
-#         first_sum += num[M]
-#     max_sum = first_sum
-#     min_sum = first_sum
 
+
+'''
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M = list(map(int, input().split()))
+    ai = list(map(int, input().split()))
+    first_num = 0
+    for k in range(M):
+        first_num += ai[k]
+    max_num = first_num
+    min_num = first_num
+    # max_num = 0
+    # min_num = 10000 * M
+    for i in range(N-M+1):
+        sum_num = 0
+        for j in range(M):
+            sum_num += ai[i+j]
+        max_num = max(sum_num, max_num)
+        min_num = min(sum_num, min_num)
+    result = max_num - min_num
+    print(f'#{tc} {result}')
