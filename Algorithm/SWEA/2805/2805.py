511bf4e4d0ac50beb7ca140af15b456415ae2d0a
yechan-EEEEE <yechan7530@gmail.com>
2025-08-19 01:08:18 +0900

    edit_2805_25.08.19

--- Algorithm/SWEA/2805/2805.py
+++ Algorithm/SWEA/2805/2805.py
@@ -16,18 +16,33 @@
 
 
 
+# T = int(input())
+
+# for tc in range(1, T + 1):
+#     N = int(input())
+#     grid = [list(map(int, input().strip())) for _ in range(N)]
+
+#     total_sum = 0
+#     center = N // 2
+
+#     for i in range(N):
+#         coord = abs(i - center)
+#         for j in range(coord, N - coord):
+#             total_sum += grid[i][j]
+    
+#     print(f'#{tc} {total_sum}')
+
+
 T = int(input())
 
-for tc in range(1, T + 1):
+for tc in range(1, T +1):
     N = int(input())
     grid = [list(map(int, input().strip())) for _ in range(N)]
-
     total_sum = 0
     center = N // 2
 
     for i in range(N):
-        coord = abs(i - center)
-        for j in range(coord, N - coord):
+        d = abs(i - center)
+        for j in range(d, N - d):
             total_sum += grid[i][j]
-    
     print(f'#{tc} {total_sum}')
