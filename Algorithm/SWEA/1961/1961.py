65aaeffce5b0416745820b3ec47aec060eca2705
yechan <yechan7530@gmail.com>
2025-08-21 02:51:12 +0900

    edit_1961

---
+++ Algorithm/SWEA/1961/1961.py
@@ -0,0 +1,22 @@
+T = int(input())
+
+for tc in range(1, T + 1):
+    N = int(input())
+    grid = [list(map(int, input().split())) for _ in range(N)]
+    rotated_grid = []
+
+    for k in range(3):  # 90도 돌리기 3번
+        new_grid = [[0]*N for _ in range(N)]  # 칸 수 똑같은 새로운 그리드
+        for i in range(N):
+            for j in range(N):
+                new_grid[j][N-i-1] = grid[i][j]  # 이동 규칙대로 이동
+        rotated_grid.append(new_grid)  # 전부 출력해야해서 돌렸을 때 값을 저장해둠
+        grid = new_grid  # 다시 돌려야하니 grid에 돌아간 값을 저장
+    # print(rotated_grid)
+    print(f'#{tc}')
+    for i in range(N):
+        result = []
+        for j in rotated_grid:
+            result.append(''.join(map(str, j[i])))  # result에 저장해둔 돌아간 값들 첫번째 줄부터 띄어쓰기를 빼고 넣어줌
+            # print(result)
+        print(' '.join(result))  # result에 들어간 값들을 서로 띄어쓰기를 해서 출력
