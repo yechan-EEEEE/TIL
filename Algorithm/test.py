6cfb60237d290fd09b79fffe2e6eaaaeb5271e7b
yechan <yechan7530@gmail.com>
2025-08-20 14:45:00 +0900

    study_25.08.20_1

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -306,25 +306,38 @@ v 1 2 3 4 5
 #
 # perm([], [1, 2, 3])
 
-import math
-for i in range(1, 4):
-    for j in range(1, 4):
-        if j != i:
-            for k in range(1, 4):
-                if k != j and k != i:
-                    print(i, j, k)
+# import math
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         if j != i:
+#             for k in range(1, 4):
+#                 if k != j and k != i:
+#                     print(i, j, k)
+#
+#
+# def perm(selected, remain, cnt):
+#     if not remain:
+#         print(selected)
+#     else:
+#         print(f"{'----' * cnt} selected: {selected}, remain: {remain}")
+#         for i in range(len(remain)):
+#             select_i = remain[i]
+#             remain_list = remain[:i] + remain[i + 1:]
+#             perm(selected + [select_i], remain_list, cnt + 1)
+#
+#
+# perm([], [1, 2, 3, 4], 1)
+# print(math.factorial(4))
+def comb(arr, n):
+    result = []
+    if n == 1:
+        return [[i] for i in arr]
+
+    for i in range(len(arr)):
+        elem = arr[i]
+        for rest in comb(arr[i + 1:], n - 1):
+            result.append([elem] + rest)
+    return result
 
 
-def perm(selected, remain, cnt):
-    if not remain:
-        print(selected)
-    else:
-        print(f"{'----' * cnt} selected: {selected}, remain: {remain}")
-        for i in range(len(remain)):
-            select_i = remain[i]
-            remain_list = remain[:i] + remain[i + 1:]
-            perm(selected + [select_i], remain_list, cnt + 1)
-
-
-perm([], [1, 2, 3, 4], 1)
-print(math.factorial(4))
+print(comb([1, 2, 3, 4], 3))
