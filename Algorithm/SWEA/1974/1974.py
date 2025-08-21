512dbd613bfc4e3e18dfcce4f7f0a6d45722ae3a
yechan <yechan7530@gmail.com>
2025-08-22 01:00:34 +0900

    solve_1974

--- Algorithm/SWEA/1974/1974.py
+++ Algorithm/SWEA/1974/1974.py
@@ -1,11 +1,41 @@
 T = int(input())
 
-dxy = [[0, 1], [1, 0]]
 for tc in range(1, T + 1):
     sudoku = [list(map(int, input().split())) for _ in range(9)]
-    wid_test, len_test, square_test = 0
-    result = 0
+    result = 1
 
     # 가로 검사
     for i in range(9):
-        nx = i +
+        num_test = set()
+        for j in range(9):
+            num_test.add(sudoku[i][j])
+        if len(num_test) != 9:
+            result = 0
+            break
+    # 세로 검사
+    for i in range(9):
+        num_test = set()
+        for j in range(9):
+            num_test.add(sudoku[j][i])
+        if len(num_test) != 9:
+            result = 0
+            break
+    
+    # 네모칸 검사
+    for i in range(3):       # 블록 x좌표 index
+        for j in range(3):   # 블록 y좌표 index
+            start_x = i * 3
+            start_y = j * 3
+            num_test = set()
+            for dx in range(3):
+                for dy in range(3):
+                    nx = start_x + dx
+                    ny = start_y + dy
+                    num_test.add(sudoku[nx][ny])
+            if len(num_test) != 9:
+                result = 0
+                break
+        if result == 0:
+            break
+
+    print(f'#{tc} {result}')
