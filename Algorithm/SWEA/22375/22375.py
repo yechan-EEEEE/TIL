ecc40f2b2c5efc314ec3a34d183bac94b38238bd
yechan-EEEEE <yechan7530@gmail.com>
2025-08-22 10:47:09 +0900

    edit_22375

--- Algorithm/SWEA/22375/22375.py
+++ Algorithm/SWEA/22375/22375.py
@@ -4,15 +4,11 @@ for tc in range(1,T+1):
     Ai = list(map(int,input().split()))
     Bi = list(map(int,input().split()))
     change_cnt = 0
-    Ci = Ai
     # print(Bi)
-    # print(Ci)
     for x in range(N):
-        if Ci[x] != Bi[x]:
+        if Ai[x] != Bi[x]:
             # print(x)
             for y in range(N-x):
-                Ci[x+y] = (Ci[x+y] + 1 )% 2
-                # print((Ci[x] + 1 )% 2)
-                # print(Ci)
+                Ai[x+y] = (Ai[x+y] + 1 )% 2
             change_cnt += 1
     print(f'#{tc} {change_cnt}')
