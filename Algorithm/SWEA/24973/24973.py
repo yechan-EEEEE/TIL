a3aaa8f6b7e6c24f55df6be0a609d6c4a68cc357
yechan-EEEEE <yechan7530@gmail.com>
2025-08-23 23:56:02 +0900

    edit_24973

--- Algorithm/SWEA/24973/24973.py
+++ Algorithm/SWEA/24973/24973.py
@@ -5,35 +5,30 @@ dxy = [[1, 0], [-1, 0], [0, -1], [0, 1]]
 for tc in range(1, T + 1):
     N = int(input())
     map_height = [list(map(int, input().split())) for _ in range(N)]
+
     highest_height = 0
-    max_mov = 0
     for x in range(N):
         for y in range(N):
             if map_height[x][y] > highest_height:
                 highest_height = map_height[x][y]
-    print(f'high={highest_height}')
+                
+    max_mov = 0
     for i in range(N):
-        mov_cnt = 1
         for j in range(N):
             if map_height[i][j] == highest_height:
-                lowest_test = 1
-                while lowest_test == 1:
-                    min_height = map_height[i][j]
+                x, y = i, j
+                mov_cnt = 1
+                while True:
+                    min_height = map_height[x][y]
+                    next_x, next_y = -1, -1
                     for dx, dy in dxy:
-                        nx = i + dx
-                        ny = j + dy
-                        print(f'min={min_height}')
+                        nx, ny = x + dx, y + dy 
                         if 0 <= nx < N and 0 <= ny < N and map_height[nx][ny] < min_height:
                             min_height = map_height[nx][ny]
-                            # print(f'min={min_height}')
-                    if min_height == map_height[i][j]:
-                        lowest_test -= 1
-                    mov_cnt += 1
-                    i, j = nx, ny
-
-        max_mov = max(max_mov, mov_cnt)
+                            next_x, next_y = nx, ny
+                    if next_x == -1:
+                        break
+                    x, y = next_x, next_y
+                    mov_cnt += 1        
+                max_mov = max(max_mov, mov_cnt)
     print(f'#{tc} {max_mov}')
-
-
-
-
