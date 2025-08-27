9d971c748ce4693eaaf1913bed14386f21d99db2
yechan-EEEEE <yechan7530@gmail.com>
2025-08-27 17:58:48 +0900

    lesson_25.08.27_2

---
+++ Algorithm/SWEA/1211/1211.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+for tc in range(10):
+    T = int(input())
+    ladder = [list(map(int, input().split())) for _ in range(100)]
+    
+    result = 0
+
+
+
+
+
+
+
+
+
+    print(f'3{T} {result}')
+
