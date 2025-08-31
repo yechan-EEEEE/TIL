cf9faac394f77ed7aff386c5f24d80c7c3806bbc
yechan <yechan7530@gmail.com>
2025-09-01 07:08:16 +0900

    test_study

--- Algorithm/SWEA/5201/5201.py
+++ Algorithm/SWEA/5201/5201.py
@@ -4,7 +4,7 @@ for tc in range(1, T + 1):
     N, M = map(int, input().split())
     wi = list(map(int, input().split()))
     ti = list(map(int, input().split()))
-    wi.sort()
+    wi.sort(reverse=True)
     wi.reverse()
     ti.sort()
     ti.reverse()
