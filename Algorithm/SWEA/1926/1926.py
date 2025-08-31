e95edef5fd90dbab41bd503a5ae37b36d0547abb
yechan-EEEEE <yechan7530@gmail.com>
2025-08-31 21:49:35 +0900

    review_1926

--- Algorithm/SWEA/1926/1926.py
+++ Algorithm/SWEA/1926/1926.py
@@ -22,26 +22,26 @@ N은 10 이상 1, 000 이하의 정수이다.(10 ≤ N ≤ 1, 000)
 
 [출력]
 1 ~ N까지의 숫자를 게임 규칙에 맞게 출력한다.
-'''
-string = [] # 빈 칸 만들어두고
-N = int(input()) # 입력을 받고
-for i in range(1, N + 1): # 1부터 입력받은 수까지 반복
-    a = str(i) # 입력받은 수를 str로 바꾸고
-    b = ' '.join(a) # str이 2글자 이상이면 숫자 사이에 ' ' 를 넣어주고
-    c = b.count('3') # b 안에 3이 있으면 d + 1
-    d = b.count('6') # b 안에 6이 있으면 e + 1
-    e = b.count('9') # b 안에 9가 있으면 f + 1
-    f = c + d + e
-    if f == 1: # 3개 합쳐서 1이면 3,6,9 중에 하나만 있으니까 - 출력
-        string.append('-') # 처음 만든 빈 리스트 안에 - 넣어주기
-    elif f == 2: # 3개 합쳐서 2면 3,6,9 중에 2개가 있는 거니까 -- 출력
-        string.append('--') # 처음 만든 빈 리스트에 -- 넣어주기
-    elif f == 3: # 3개 합쳐서 3이면 세자리 수가 다 3,6,9인 거니까 --- 출력
-        string.append('---') # 처음 만든 빈 리스트에 --- 넣어주기
-    elif f == 0: # 3개 합쳐서 0이면 아무 것도 아니니깐
-        string.append(str(i)) # 지금 자기 숫자 빈 리스트에 추가
-result = " ".join(string) # 숫자들 사이에 ' ' 넣어주기
-print(result)
+# '''
+# string = [] # 빈 칸 만들어두고
+# N = int(input()) # 입력을 받고
+# for i in range(1, N + 1): # 1부터 입력받은 수까지 반복
+#     a = str(i) # 입력받은 수를 str로 바꾸고
+#     b = ' '.join(a) # str이 2글자 이상이면 숫자 사이에 ' ' 를 넣어주고
+#     c = b.count('3') # b 안에 3이 있으면 d + 1
+#     d = b.count('6') # b 안에 6이 있으면 e + 1
+#     e = b.count('9') # b 안에 9가 있으면 f + 1
+#     f = c + d + e
+#     if f == 1: # 3개 합쳐서 1이면 3,6,9 중에 하나만 있으니까 - 출력
+#         string.append('-') # 처음 만든 빈 리스트 안에 - 넣어주기
+#     elif f == 2: # 3개 합쳐서 2면 3,6,9 중에 2개가 있는 거니까 -- 출력
+#         string.append('--') # 처음 만든 빈 리스트에 -- 넣어주기
+#     elif f == 3: # 3개 합쳐서 3이면 세자리 수가 다 3,6,9인 거니까 --- 출력
+#         string.append('---') # 처음 만든 빈 리스트에 --- 넣어주기
+#     elif f == 0: # 3개 합쳐서 0이면 아무 것도 아니니깐
+#         string.append(str(i)) # 지금 자기 숫자 빈 리스트에 추가
+# result = " ".join(string) # 숫자들 사이에 ' ' 넣어주기
+# print(result)
 # N = int(input())
 
 # for i in range(1, N+1):
@@ -103,3 +103,10 @@ if b == 3 or b == 6 or b == 9:
         print('--')
 print(f'{a}')
 '''
+N = int(input())
+for i in range(1, N+1):
+    if '3' in str(i) or '6' in str(i) or '9' in str(i):
+        cnt = str(i).count('3') + str(i).count('6') + str(i).count('9')
+        print('-' * cnt, end=' ')
+    else:
+        print(i, end=' ')
