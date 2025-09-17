17890f978fb63d22469d41e789bb7199097c71a9
yechan-EEEEE <yechan7530@gmail.com>
2025-09-17 17:57:07 +0900

    class_25.09.17

---
+++ Algorithm/SWEA/4871/4871.py
@@ -0,0 +1,33 @@
+T = int(input())
+
+for tc in range(1, T + 1):
+    V, E = map(int, input().split())
+
+    graph = [[] for _ in range(V + 1)]
+
+    for i in range(E):
+        a, b = map(int, input(). split())
+        graph[a].append(b)
+
+    # print(graph)
+
+    S, G = map(int, input().split())
+
+    visited = [0] * (V + 1)
+    ans = 0
+
+    def dfs(node):
+        if node == G:
+            return True
+        visited[node] = 1
+        for n_node in graph[node]:
+            if not visited[n_node]:
+                if dfs(n_node):
+                    return True
+        return False
+
+    if dfs(S):
+        ans = 1
+    else:
+        ans = 0
+    print(f'#{tc} {ans}')
