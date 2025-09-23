17396baf05fef2f26e3c0fd0c01a9a36d4a260da
yechan <yechan7530@gmail.com>
2025-09-23 22:00:37 +0900

    final_3289

--- Algorithm/SWEA/3289/3289.py
+++ Algorithm/SWEA/3289/3289.py
@@ -1,38 +1,70 @@
+# def find_set(x):
+#     if x != p_list[x]:
+#         p_list[x] = find_set(p_list[x])
+#     return p_list[x]
+
+
+# def union(x, y):
+#     px = find_set(x)
+#     py = find_set(y)
+
+#     if px != py:
+#         if rank[px] > rank[py]:
+#             p_list[py] = px
+#         elif rank[px] < rank[py]:
+#             p_list[px] = py
+#         else:
+#             p_list[py] = px
+#             rank[px] += 1
+
+
+# def check(x, y):
+#     px = find_set(x)
+#     py = find_set(y)
+
+#     if px != py:
+#         return 0
+#     else:
+#         return 1
+
+
+# T = int(input())
+# for tc in range(1, T + 1):
+#     n, m = map(int, input().split())
+#     p_list = list(range(n + 1))
+#     rank = list(range(n + 1))
+#     ans = ''
+#     for i in range(m):
+#         function, a, b = map(int, input().split())
+#         if function == 0:
+#             union(a, b)
+#         elif function == 1:
+#             A = str(check(a, b))
+#             ans += A
+
+#     # print(f'#{tc} {"".join(ans)}')
+#     print(f'#{tc} {ans}')
 def find_set(x):
+    if x != p[x]:
+        p[x] = find_set(p[x])
+    return p[x]
 
-    if x != p_list[x]:
-        p_list[x] = find_set(p_list[x])
-    return p_list[x]
 
-
-def union(x, y):
-    # 최적화 전
-    # px = find_set(x)
-    # py = find_set(y)
-    #
-    # if px != py:
-    #     if px < py:
-    #         p_list[y] = px
-    #     else:
-    #         p_list[x] = py
-
-    # 최적화
-    px = find_set(x)
-    py = find_set(y)
+def union_set(x, y):
+    px, py = find_set(x), find_set(y)
 
     if px != py:
         if rank[px] > rank[py]:
-            p_list[py] = px
+            p[py] = px
         elif rank[px] < rank[py]:
-            p_list[px] = py
+            p[px] = py
         else:
-            p_list[py] = px
+            p[py] = px
             rank[px] += 1
 
 
 def check(x, y):
-    px = find_set(x)
-    py = find_set(y)
+    px, py = find_set(x), find_set(y)
 
     if px != py:
         return 0
@@ -43,16 +75,16 @@ def check(x, y):
 T = int(input())
 for tc in range(1, T + 1):
     n, m = map(int, input().split())
-    p_list = list(range(n + 1))
-    rank = list(range(n + 1))
+    p = list(range(n + 1))
+    rank = [0] * (n + 1)
     ans = ''
+
     for i in range(m):
         function, a, b = map(int, input().split())
         if function == 0:
-            union(a, b)
-        elif function == 1:
-            A = str(check(a, b))
-            ans += A
-
-    # print(f'#{tc} {"".join(ans)}')
+            union_set(a, b)
+        if function == 1:
+            A = check(a, b)
+            ans += str(A)
+    
     print(f'#{tc} {ans}')
