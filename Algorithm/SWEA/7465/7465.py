0416b53aa2fcba953fb445abb00be371c0673d91
yechan-EEEEE <yechan7530@gmail.com>
2025-09-28 22:24:09 +0900

    25.09.28_study

--- Algorithm/SWEA/7465/7465.py
+++ Algorithm/SWEA/7465/7465.py
@@ -33,36 +33,6 @@
 
 #     print(f'#{tc} {len(set(p_list))-1}')
 
-# def find_set(x):
-#     if x != p[x]:
-#         p[x] = find_set(p[x])
-#     return p[x]
-
-
-# def union_set(x, y):
-#     px = find_set(x)
-#     py = find_set(y)
-
-#     if px != py:
-#         if rank[px] > rank[py]:
-#             p[py] = px
-#         elif rank[px] < rank[py]:
-#             p[px] = py
-#         else:
-#             p[py] = px
-#             rank[px] += 1
-
-# T = int(input())
-# for tc in range(1, T + 1):
-#     N, M = map(int, input().split())
-#     p = list(range(N + 1))
-#     rank = [0] * (N + 1)
-    
-#     for i in range(M):
-#         a, b = map(int, input().split())
-#         union_set(a, b)
-    
-#     print(f'#{tc} {len(set(p)) - 1}')
 
 def find_set(x):
     if x != p[x]:
