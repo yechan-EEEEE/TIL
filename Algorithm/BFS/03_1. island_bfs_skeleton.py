60fcc582e8b79879c3885c869ff2a9967990f8a4
yechan-EEEEE <yechan7530@gmail.com>
2025-09-30 21:16:27 +0900

    edit_island_problem

--- Algorithm/BFS/03_1. island_bfs_skeleton.py
+++ Algorithm/BFS/03_1. island_bfs_skeleton.py
@@ -72,84 +72,45 @@
 # # print(island_cnt)  # 섬의 개수 출력
 
 
-# from collections import deque
-# import math
-# dxy = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, 1), (1, 1), (1, -1), (-1, -1)]
-#
-#
-# def find_island(Map, x, y):
-#     global max_cnt, min_cnt, island_species, sum_island
-#     queue = deque()
-#     queue.append((x, y))
-#     island_species.add(Map[x][y])
-#     Map[x][y] = '0'
-#
-#     while queue:
-#         cx, cy = queue.popleft()
-#         sum_island += 1
-#         for dx, dy in dxy:
-#             nx, ny = cx + dx, cy + dy
-#             if 0 <= nx < N and 0 <= ny < M:
-#                 if Map[nx][ny] == '0':
-#                     continue
-#                 if Map[nx][ny] != '0':
-#                     find_island(Map, nx, ny)
-#         max_cnt = max(sum_island, max_cnt)
-#         min_cnt = min(sum_island, min_cnt)
-#
-#
-# T = int(input())
-# for tc in range(1, T + 1):
-#     N, M = map(int, input().split())
-#     map_info = [list(map(str, input().split())) for _ in range(M)]
-#     max_cnt = 0
-#     min_cnt = math.inf
-#     sum_island = 0
-#     island_species = set()
-#
-#     for i in range(N):
-#         for j in range(M):
-#             if map_info[i][j] == 0:
-#                 continue
-#             find_island(map_info, i, j)
-#     print(max_cnt, min_cnt, len(island_species)-1)
 from collections import deque
 import math
 dxy = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, 1), (1, 1), (1, -1), (-1, -1)]
 
 
 def find_island(Map, x, y):
+    global cnt
     queue = deque()
     queue.append((x, y))
-    island_species.add(Map[x][y])
-    Map[x][y] = '0'
-    size = 1
+    island_species.add(Map[x][y])  # 종류 세려고 set에 입력
+    Map[x][y] = '0'  # 확인한 곳은 0으로 바꿔서 pass
+    cnt += 1  # 함수를 실행하면 그 곳부터 섬의 크기니까 +1
 
     while queue:
         cx, cy = queue.popleft()
         for dx, dy in dxy:
             nx, ny = cx + dx, cy + dy
-            if 0 <= nx < N and 0 <= ny < M and Map[nx][ny] != '0':
+            if 0 <= nx < N and 0 <= ny < M and Map[nx][ny] != '0':  # 지도 안이고 0이 아니면 섬이다
                 queue.append((nx, ny))
-                Map[nx][ny] = '0'
-                size += 1
-    return size
+                Map[nx][ny] = '0'  # 방문했으니 0으로 처리
+                cnt += 1  # 크기 +1
 
 
 T = int(input())
 for tc in range(1, T + 1):
     N, M = map(int, input().split())
-    map_info = [input().split() for _ in range(N)]
-    max_cnt = 0
-    min_cnt = math.inf
-    island_species = set()
+    map_info = [input().split() for _ in range(N)]  # map 정보
+    cnt = 0  # 섬 크기 저장할 변수
+    max_cnt = 0  # 제일 큰 섬 크기 저장할 변수
+    min_cnt = math.inf  # 제일 작은 섬 크기 저장할 변수
+    island_species = set()  # 섬 종류 저장할 변수
 
     for i in range(N):
         for j in range(M):
-            if map_info[i][j] == '0':
+            cnt = 0  # 새로운 칸에서 시작할 때마다 크기 초기화
+            if map_info[i][j] == '0':  # 0이면 섬 아니니까 pass
                 continue
-            size = find_island(map_info, i, j)
-            max_cnt = max(max_cnt, size)
-            min_cnt = min(min_cnt, size)
+            find_island(map_info, i, j)  # 0이 아니면 섬이니까 함수 실행
+            max_cnt = max(max_cnt, cnt)  # 함수 끝나고 max와 min 갱신
+            min_cnt = min(min_cnt, cnt)
 
-    print(max_cnt - min_cnt, len(island_species))
+    print(f'#{tc} {max_cnt - min_cnt} {len(island_species)}')
