b658a084f2103bb196171ea45e56980e77638337
azure-553 <yechan7530@gmail.com>
2026-01-14 16:39:07 +0900

    solve_BJ_8383

--- Algorithm/BAEKJOON/test.py
+++ Algorithm/BAEKJOON/test.py
@@ -1,4 +1,7 @@
-T = int(input())
-for t in range(1, T + 1):
-    A, B = map(int, input().split())
-    print(A + B)
+n = int(input())
+result = 0
+
+for i in range(1, n+1):
+    result += i
+
+print(result)
