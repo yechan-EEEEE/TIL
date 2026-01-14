2d12455c432e1f6f373d4ed1da4902a2fc0d2868
azure-553 <yechan7530@gmail.com>
2026-01-14 16:48:14 +0900

    solve_BJ_25304

--- Algorithm/BAEKJOON/25304.py
+++ Algorithm/BAEKJOON/25304.py
@@ -11,15 +11,3 @@
 출력
 구매한 물건의 가격과 개수로 계산한 총 금액이 영수증에 적힌 총 금액과 일치하면 Yes를 출력한다. 일치하지 않는다면 No를 출력한다.
 """
-X = int(input())
-N = int(input())
-total = 0
-
-for i in range(1, N + 1):
-    a, b = map(int, input().split())
-    total += a * b
-
-if X == total:
-    print("Yes")
-else:
-    print("No")
