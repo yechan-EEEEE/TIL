e4cec86e6e0ceb0d61b8b8d15c95062ad61326ea
azure-553 <yechan7530@gmail.com>
2026-01-15 17:37:08 +0900

    BJ_10807_ing

--- Algorithm/BAEKJOON/test.py
+++ Algorithm/BAEKJOON/test.py
@@ -3,21 +3,18 @@ import sys
 input() > sys.stdin.readline()
 """
 """
-두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+총 N개의 정수가 주어졌을 때, 정수 v가 몇 개인지 구하는 프로그램을 작성하시오.
 
 입력
-입력은 여러 개의 테스트 케이스로 이루어져 있다.
-
-각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+첫째 줄에 정수의 개수 N(1 ≤ N ≤ 100)이 주어진다. 둘째 줄에는 정수가 공백으로 구분되어져있다.
+셋째 줄에는 찾으려고 하는 정수 v가 주어진다. 입력으로 주어지는 정수와 v는 -100보다 크거나 같으며, 100보다 작거나 같다.
 
 출력
-각 테스트 케이스마다 A+B를 출력한다.
+첫째 줄에 입력으로 주어진 N개의 정수 중에 v가 몇 개인지 출력한다.
 """
 import sys
 
-while True:
-    try:
-        A, B = map(int, sys.stdin.readline().split())
-        print(A + B)
-    except ValueError:
-        break
+N = int(sys.stdin.readline())
+n_list = []
+n_list.append(map(int, sys.stdin.readline().split()))
+v = int(sys.stdin.readline())
