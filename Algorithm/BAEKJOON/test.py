b265748ce66e202864f54b49d008901b3dcf9e73
azure-553 <yechan7530@gmail.com>
2026-01-22 17:54:05 +0900

    ing_SWEA_25697

--- Algorithm/BAEKJOON/test.py
+++ Algorithm/BAEKJOON/test.py
@@ -1,8 +1,52 @@
 """
 
+5
+
+4 10
+1 2 3 4
+            4
+4 11
+1 2 3 4
+            5
+1 3
+5
+            2
+6 6
+1 3 5 2 4 6
+            3
+6 10000000
+1 3 5 2 4 6
+            2857144
 """
 import sys
 input = sys.stdin.readline
-
-height = float(input())
-print(height-0.3)
+135246
+x = 10000000
+y = 0
+cnt = 0
+while True:
+    y+=1
+    cnt+=1
+    if y>x:
+        break
+    y+=3
+    cnt+=1
+    if y>x:
+        break
+    y+=5
+    cnt+=1
+    if y>x:
+        break
+    y+=2
+    cnt+=1
+    if y>x:
+        break
+    y+=4
+    cnt+=1
+    if y>x:
+        break
+    y+=6
+    cnt+=1
+    if y>x:
+        break
+print(cnt)
---
+++ Algorithm/SWEA/25697/25697.py
@@ -0,0 +1,39 @@
+"""
+5
+4 10
+1 2 3 4
+            4
+4 11
+1 2 3 4
+            5
+1 3
+5
+            2
+6 6
+1 3 5 2 4 6
+            3
+6 10000000
+1 3 5 2 4 6
+            2857144
+"""
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    N, X = map(int, input().split())
+    d_list = list(map(int, input().split()))
+    cnt = 0
+    cur_distance = 0
+    if len(d_list) == 1 and d_list[0] > abs(X):
+        cur_distance += d_list[0]
+        cnt += 1
+    while True:        
+        for i in d_list:
+            cur_distance += i
+            cnt += 1
+            if cur_distance >= abs(X):
+                break
+        if cur_distance >= abs(X):
+            break
+    print(cnt)
