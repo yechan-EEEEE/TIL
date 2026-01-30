30ea3a1dba460632072f82e179312e1ce3dc9406
문예찬 <yechan7530@gmail.com>
2026-01-30 17:32:59 +0900

    solve_BJ_2231

---
+++ Algorithm/BAEKJOON/2231.py
@@ -0,0 +1,41 @@
+"""
+어떤 자연수 N이 있을 때, 그 자연수 N의 분해합은 N과 N을 이루는 각 자리수의 합을 의미한다.
+어떤 자연수 M의 분해합이 N인 경우, M을 N의 생성자라 한다.
+예를 들어, 245의 분해합은 256(=245+2+4+5)이 된다.
+따라서 245는 256의 생성자가 된다.
+물론, 어떤 자연수의 경우에는 생성자가 없을 수도 있다.
+반대로, 생성자가 여러 개인 자연수도 있을 수 있다.
+
+자연수 N이 주어졌을 때, N의 가장 작은 생성자를 구해내는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 자연수 N(1 ≤ N ≤ 1,000,000)이 주어진다.
+
+출력
+첫째 줄에 답을 출력한다. 생성자가 없는 경우에는 0을 출력한다.
+
+N의 자릿수 구하기
+
+9 × 자릿수 계산
+
+N - 그 값부터 N-1까지만 검사
+"""
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+str_N = str(N)
+length = len(str_N)
+start = N - (9 * length)
+if start < 1:
+    start = 1
+result = 0
+
+for i in range(start, N):
+    sum_value = i
+    for j in str(i):
+        sum_value += int(j)
+    if sum_value == N:
+        result = i
+        break
+print(result)
